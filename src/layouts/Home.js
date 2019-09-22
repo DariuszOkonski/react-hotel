2ea2f30f1d80780f3456e50c1ea2eaa8c27355d0
Dariusz Okonski <darek200180@gmail.com>
2019-09-22 19:32:48 +0100

    import HomeInfo component and add to structure

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Showcase from '../components/Showcase';
+import HomeInfo from '../components/HomeInfo';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Showcase />
+      <HomeInfo />
     </React.Fragment>
   );
 }
