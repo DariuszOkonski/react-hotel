d96daac5594adb956039f9900288ddb6aae20d74
Dariusz Okonski <darek200180@gmail.com>
2019-09-22 17:15:51 +0100

    add components Home, About, Contact, Error and implement routing at component App

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navigation from './Navigation';
+import Home from '../layouts/Home';
+import About from '../layouts/About';
+import Contact from '../layouts/Contact';
+import Error from '../layouts/Error';
 
 function App() {
   return (
@@ -8,6 +12,13 @@ function App() {
       <React.Fragment>
 
         <Navigation />
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route component={Error} />
+        </Switch>
 
       </React.Fragment>
     </BrowserRouter>
