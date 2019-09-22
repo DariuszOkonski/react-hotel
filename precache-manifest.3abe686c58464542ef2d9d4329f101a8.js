11e7395b2fdd74be5e445827a3c588ab99a29b9a
Dariusz Okonski <darek200180@gmail.com>
2019-09-22 15:21:06 +0100

    Updates

---
+++ precache-manifest.3abe686c58464542ef2d9d4329f101a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec76364e8090287df7c324950cb7a839",
+    "url": "/react-hotel/index.html"
+  },
+  {
+    "revision": "8575e4aa870d88103b3f",
+    "url": "/react-hotel/static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "2d16e7c7f6468ba29794",
+    "url": "/react-hotel/static/js/2.0ce48339.chunk.js"
+  },
+  {
+    "revision": "8575e4aa870d88103b3f",
+    "url": "/react-hotel/static/js/main.813c2e58.chunk.js"
+  },
+  {
+    "revision": "da2fde78a7350fd379b3",
+    "url": "/react-hotel/static/js/runtime-main.ffaee524.js"
+  }
+]);
---
+++ static/js/main.813c2e58.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp009-hotel"]=window["webpackJsonp009-hotel"]||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var o=t(0),a=t.n(o),r=t(2),c=t.n(r);t(8);var l=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"Hotel"))};c.a.render(a.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.813c2e58.chunk.js.map
