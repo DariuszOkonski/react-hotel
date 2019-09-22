0be25ed9c3789dba0f13a8428de905053b38eb69
Dariusz Okonski <darek200180@gmail.com>
2019-09-22 17:00:19 +0100

    add basename to BrowserRouter

--- src/views/App.js
+++ src/views/App.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <React.Fragment>
 
         <Navigation />
