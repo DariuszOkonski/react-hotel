0011797bfbdb397c0479281e531d581f1d06e36a
Dariusz Okonski <darek200180@gmail.com>
2019-09-30 17:26:39 +0100

    Add component Features with features.scss

---
+++ src/components/Features.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Features = () => {
+  return (
+    <section className="features">
+
+      <div className="box">
+        <i class="fa fa-building box__icon" aria-hidden="true"></i>
+        <h4 className="box__header">Great Location</h4>
+        <p className="box__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veritatis amet illum ab culpa pariatur nemo eum maiores ullam veniam?</p>
+      </div>
+
+      <div className="box">
+        <i class="fa fa-cutlery box__icon" aria-hidden="true"></i>
+        <h4 className="box__header">Free Meals</h4>
+        <p className="box__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veritatis amet illum ab culpa pariatur nemo eum maiores ullam veniam?</p>
+      </div>
+
+      <div className="box">
+        <i class="fa fa-bicycle box__icon" aria-hidden="true"></i>
+        <h4 className="box__header">Fitness Room</h4>
+        <p className="box__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veritatis amet illum ab culpa pariatur nemo eum maiores ullam veniam?</p>
+      </div>
+
+
+    </section>
+  );
+}
+
+export default Features;
