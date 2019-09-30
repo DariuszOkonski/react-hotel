126c6bf753db391fa6a35c2d58e082b0bd3ddc4f
Dariusz Okonski <darek200180@gmail.com>
2019-09-30 17:24:27 +0100

    Add component Features to Home component

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Showcase from '../components/Showcase';
 import HomeInfo from '../components/HomeInfo';
+import Features from '../components/Features';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Showcase />
       <HomeInfo />
+      <Features />
     </React.Fragment>
   );
 }
