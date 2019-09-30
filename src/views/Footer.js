b716f30f83500bd608c4f67cc00bb6c1576934a4
Dariusz Okonski <darek200180@gmail.com>
2019-09-30 17:41:39 +0100

    add Footer view and footer.scss styles for mobile and desktop view

---
+++ src/views/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p className='footer__paragraph'>Hotel Royal &#169; 2019 Dariusz Okonski</p>
+    </footer>
+  );
+}
+
+export default Footer;
