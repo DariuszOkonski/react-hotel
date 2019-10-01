b57e7c665b6680357904be1fc48baa8662c6cf02
Dariusz Okonski <darek200180@gmail.com>
2019-10-01 10:21:16 +0100

    add AboutGuests component to About component

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutInfo from '../components/AboutInfo';
+import AboutGuests from '../components/AboutGuests';
 
 const About = () => {
   return (
     <React.Fragment>
       <AboutInfo />
+      <AboutGuests />
     </React.Fragment>
   );
 }
