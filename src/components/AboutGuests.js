147c1c3ba535675f765cd61929f79d6c4eabeb47
Dariusz Okonski <darek200180@gmail.com>
2019-10-01 10:41:46 +0100

    add testimonialsList and display it by map method

--- src/components/AboutGuests.js
+++ src/components/AboutGuests.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import face1 from '../img/face1.jpg';
+import face2 from '../img/face2.jpg';
+import Testimonial from '../components/Testimonial';
+
+const testiomnialsList = [
+  { id: 1, picture: face2, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis error eius sunt totam corrupti accusamus modi aspernatur sapiente labore reiciendis iure porro quo tempore recusandae dolorum doloribus numquam, praesentium, ullam velit optio veniam maxime laboriosam. Nemo eius iure porro?' },
+  { id: 2, picture: face1, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis error eius sunt totam corrupti accusamus modi aspernatur sapiente labore reiciendis iure porro quo tempore recusandae dolorum doloribus numquam, praesentium, ullam velit optio veniam maxime laboriosam. Nemo eius iure porro?' },
+]
 
 const AboutGuests = () => {
   return (
     <section className="about-guests">
       <h2 className="about-guests__header container">What Our Guest Say</h2>
 
-      <div className="testimonial container">
-        <div className="testimonial__image">
-          <img src={face1} alt="face" />
-        </div>
-        <div className="testimonial__paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis error eius sunt totam corrupti accusamus modi aspernatur sapiente labore reiciendis iure porro quo tempore recusandae dolorum doloribus numquam, praesentium, ullam velit optio veniam maxime laboriosam. Nemo eius iure porro?
-        </div>
-      </div>
-
-      <div className="testimonial container">
-        <div className="testimonial__image">
-          <img src={face1} alt="face" />
-        </div>
-        <div className="testimonial__paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis error eius sunt totam corrupti accusamus modi aspernatur sapiente labore reiciendis iure porro quo tempore recusandae dolorum doloribus numquam, praesentium, ullam velit optio veniam maxime laboriosam. Nemo eius iure porro?
-        </div>
-      </div>
+      {testiomnialsList.map(testimonial => <Testimonial key={testimonial.id} {...testimonial} />)}
 
     </section>
   );
