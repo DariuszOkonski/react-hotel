c1a8004b2211beb951c810f26bdb437e60d9d39b
Dariusz Okonski <darek200180@gmail.com>
2019-10-01 14:17:02 +0100

    fix small bug, class change into className

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,19 +5,19 @@ const Features = () => {
     <section className="features">
 
       <div className="box">
-        <i class="fa fa-building box__icon" aria-hidden="true"></i>
+        <i className="fa fa-building box__icon" aria-hidden="true"></i>
         <h4 className="box__header">Great Location</h4>
         <p className="box__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veritatis amet illum ab culpa pariatur nemo eum maiores ullam veniam?</p>
       </div>
 
       <div className="box">
-        <i class="fa fa-cutlery box__icon" aria-hidden="true"></i>
+        <i className="fa fa-cutlery box__icon" aria-hidden="true"></i>
         <h4 className="box__header">Free Meals</h4>
         <p className="box__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veritatis amet illum ab culpa pariatur nemo eum maiores ullam veniam?</p>
       </div>
 
       <div className="box">
-        <i class="fa fa-bicycle box__icon" aria-hidden="true"></i>
+        <i className="fa fa-bicycle box__icon" aria-hidden="true"></i>
         <h4 className="box__header">Fitness Room</h4>
         <p className="box__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veritatis amet illum ab culpa pariatur nemo eum maiores ullam veniam?</p>
       </div>
