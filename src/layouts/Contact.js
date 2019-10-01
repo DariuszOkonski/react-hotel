ffd3881b131fe7db5684091ed3377951cbb5e5b7
Dariusz Okonski <darek200180@gmail.com>
2019-10-01 14:40:42 +0100

    add ContactBoxes component to Contact layout

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContactForm from '../components/ContactForm';
+import ContactBoxes from '../components/ContactBoxes';
 
 const Contact = () => {
   return (
     <React.Fragment>
       <ContactForm />
+      <ContactBoxes />
     </React.Fragment>
   );
 }
